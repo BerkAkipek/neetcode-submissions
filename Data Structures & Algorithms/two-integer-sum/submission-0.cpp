40e85804363ee9490b0728644d964f0f4ffe8294
Berk Akipek <38300766+BerkAkipek@users.noreply.github.com>
2026-05-19 19:12:08 +0300

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,20 @@
+#include <unordered_map>
+
+using std::unordered_map;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> complements;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int complement = target - nums[i];
+
+            if (complements.find(complement) != complements.end())
+                return {complements[complement], i};
+            
+            complements[nums[i]] = i;
+        }
+    }
+};
